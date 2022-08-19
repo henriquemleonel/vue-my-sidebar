5eb6bcc90488eaacc3fe29af702ffd9bd37b243f
Henrique Leonel <26604294+henriquemleonel@users.noreply.github.com>
2022-08-19 19:08:23 -0300

    fix: over-scroll interaction for scroll-wrapper component

--- src/components/ScrollWrapper.vue
+++ src/components/ScrollWrapper.vue
@@ -8,7 +8,7 @@ export default {
       direction: '',
 
       mark: {
-        top: 50,
+        top: 25,
         bottom: 50
       },
     }
@@ -53,29 +53,33 @@ export default {
       @scroll="onScroll"
   >
     <transition name="appear-up">
-      <button
-          class="vsm--top-mark"
-          v-if="direction === 'down'"
-          @click="onScrollUp"
-      >
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M5 16.929L6.41421 18.3432L12.0711 12.6863L17.7279 18.3432L19.1421 16.929L12.0711 9.85789L5 16.929Z" fill="currentColor" /><path d="M19 8H5V6H19V8Z" fill="currentColor" />
-        </svg>
-      </button>
+      <div class="vsm--scroll-anchor top-anchor">
+        <button
+            class="vsm--scroll-setter"
+            v-if="direction === 'down'"
+            @click="onScrollUp"
+        >
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5 16.929L6.41421 18.3432L12.0711 12.6863L17.7279 18.3432L19.1421 16.929L12.0711 9.85789L5 16.929Z" fill="currentColor" /><path d="M19 8H5V6H19V8Z" fill="currentColor" />
+          </svg>
+        </button>
+      </div>
     </transition>
 
     <slot />
 
     <transition name="appear-down">
-      <button
-          class="vsm--bottom-mark"
-          v-if="direction === 'up'"
-          @click="onScrollDown"
-      >
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M5 7.41421L6.41421 6L12.0711 11.6569L17.7279 6L19.1421 7.41421L12.0711 14.4853L5 7.41421Z" fill="currentColor" /><path d="M19 16.3432H5V18.3432H19V16.3432Z" fill="currentColor" />
-        </svg>
-      </button>
+      <div class="vsm--scroll-anchor bottom-anchor">
+        <button
+            class="vsm--scroll-setter"
+            v-if="direction === 'up'"
+            @click="onScrollDown"
+        >
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5 7.41421L6.41421 6L12.0711 11.6569L17.7279 6L19.1421 7.41421L12.0711 14.4853L5 7.41421Z" fill="currentColor" /><path d="M19 16.3432H5V18.3432H19V16.3432Z" fill="currentColor" />
+          </svg>
+        </button>
+      </div>
     </transition>
   </div>
 </template>
